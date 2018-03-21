110c3934c2614c94feff9b37ea12898904f2337c
BenEmdon <benjaminemdon@gmail.com>
2018-03-21 16:27:52 -0400

    Added note enum

--- 8BitMusicMaker.playground/Sources/Note.swift
+++ 8BitMusicMaker.playground/Sources/Note.swift
@@ -1,2 +1,19 @@
-import Foundation
-
+/// An enum that defines each notes pitch relative to the note C.
+/// This assumes that the provided sound assets are a C note.
+///
+/// - C: Do
+/// - D: Re
+/// - E: Mi
+/// - F: Fa
+/// - G: Sol
+/// - A: La
+/// - B: Ti
+public enum Note: Double {
+	case C = 0
+	case D = 10
+	case E = 20
+	case F = 25
+	case G = 35
+	case A = 45
+	case B = 55
+}
