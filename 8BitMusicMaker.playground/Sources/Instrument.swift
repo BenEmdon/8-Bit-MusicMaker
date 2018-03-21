379b67724eed77453acd3b5d38f0ad464005baa1
BenEmdon <benjaminemdon@gmail.com>
2018-03-21 16:28:05 -0400

    Added instrument samples

--- 8BitMusicMaker.playground/Sources/Instrument.swift
+++ 8BitMusicMaker.playground/Sources/Instrument.swift
@@ -1,2 +1,13 @@
 import Foundation
 
+/// Each instriment is a sample of a different 8-bit wave form.
+/// All samples are each wave form as a C2 note.
+///
+/// - square: square wave form.
+/// - triangle: traingle wave from.
+/// - noise: noisy wave. Imagine a ragged mountain surface.
+enum Instrument: String {
+	case square
+	case triangle
+	case noise
+}
