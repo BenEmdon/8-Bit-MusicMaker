a3a8b76fc5318c5f6b11c88b95e77a9d779a78c7
BenEmdon <benjaminemdon@gmail.com>
2018-03-24 16:35:06 -0400

    Built out sequencer to sequence notes

--- 8BitMusicMaker.playground/Contents.swift
+++ 8BitMusicMaker.playground/Contents.swift
@@ -18,19 +18,31 @@ extension NoteAtBlock: Hashable {
 	}
 }
 
-protocol SequencerDelegate: class {}
+protocol SequencerDelegate: class {
+	func blockChanged(_ block: Int)
+	func sequencerModeChanged(_ mode: Sequencer.Mode)
+}
 
 class Sequencer {
 
-	// constants
+	enum Mode {
+		case playing
+		case stopped
+	}
+
+	// Constants
 	let blocks = 12
+
+//	let blocksPerSecond: DispatchTime = 0.5
 
 	// AVFoundation dependancies
 	let engine = AVAudioEngine()
 
 	// Organized state of the sequencer
 	let players: [Instrument: [Note: PitchedPlayer]]
-	var notesAtBlocks: [Instrument: Set<NoteAtBlock>]
+	public private(set) var notesAtBlocks: [Instrument: Set<NoteAtBlock>]
+	var notesPlaying = [Instrument: Note]()
+	var currentMode: Mode = .stopped
 
 	weak var delegate: SequencerDelegate?
 
@@ -40,10 +52,7 @@ class Sequencer {
 		notesAtBlocks = state
 	}
 
-	func start() {
-		engine.prepare()
-		try! engine.start()
-	}
+	// MARK: Static dispatched setup functions
 
 	private static func audioBuffers(for instruments: [Instrument]) -> [Instrument: AVAudioPCMBuffer] {
 		var audioBuffers = [Instrument: AVAudioPCMBuffer]()
@@ -69,26 +78,96 @@ class Sequencer {
 		return allPlayers
 	}
 
+	// MARK: Sequencer behavior
+
+	func prepareForPlaying() {
+		engine.prepare()
+		try! engine.start()
+	}
+
+	func hardStop() {
+		currentMode = .stopped
+		for (instrument, notesAtBlocks) in notesAtBlocks {
+			let notesInBlock = notesAtBlocks
+				.map { noteAtBlock in noteAtBlock.note }
+			print(notesInBlock)
+			notesInBlock.forEach { note in stopNote(note, onInstrument: instrument) }
+		}
+	}
+
+	func start() {
+		currentMode = .playing
+		sequenceNotes(forNewBlock: 0, oldBlock: nil)
+	}
+
+	func sequenceNotes(forNewBlock newBlock: Int, oldBlock: Int?) {
+		if let oldBlock = oldBlock {
+			stopNotesForBlock(oldBlock)
+		}
+		guard currentMode == .playing else { return }
+		delegate?.blockChanged(newBlock)
+		playNotesForBlock(newBlock)
+		DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [weak self] in
+			guard let blocks = self?.blocks else { return }
+			let allButLastBlock = 0..<(blocks - 1)
+			if allButLastBlock.contains(newBlock) {
+				self?.sequenceNotes(forNewBlock: newBlock + 1, oldBlock: newBlock)
+			} else {
+				self?.sequenceNotes(forNewBlock: 0, oldBlock: newBlock)
+			}
+		}
+	}
+
+	private func playNotesForBlock(_ block: Int) {
+		for (instrument, notesAtBlocks) in notesAtBlocks {
+			let notesInBlock = notesAtBlocks
+				.filter { noteAtBlock in noteAtBlock.block == block }
+				.map { noteAtBlock in noteAtBlock.note }
+			notesInBlock.forEach { note in playNote(note, onInstrument: instrument) }
+
+		}
+	}
+
+	private func stopNotesForBlock(_ block: Int) {
+		for (instrument, notesAtBlocks) in notesAtBlocks {
+			let notesInBlock = notesAtBlocks
+				.filter { noteAtBlock in noteAtBlock.block == block }
+				.map { noteAtBlock in noteAtBlock.note }
+			notesInBlock.forEach { note in stopNote(note, onInstrument: instrument) }
+		}
+	}
+
 	func registerNote(_ note: Note, onInstrument instrument: Instrument, forBlock block: Int) {
 	}
 
 	func playNote(_ note: Note, onInstrument instrument: Instrument) {
 		// simple play a note
-		if let player = players[instrument]?[note] {
-			player.play()
-		}
+		players[instrument]![note]!.play()
 	}
 
 	func stopNote(_ note: Note, onInstrument instrument: Instrument) {
 		// simple play a note
-		if let player = players[instrument]?[note] {
-			player.stop()
-		}
+		players[instrument]![note]!.stop()
 	}
 }
 
 class View: UIView {
-	let sequencer = Sequencer(with: [.square])
+	let sequencer = Sequencer(with: [.square], initialState: [
+		.square: [
+			NoteAtBlock(note: .C, block: 0),
+			NoteAtBlock(note: .D, block: 1),
+			NoteAtBlock(note: .E, block: 2),
+			NoteAtBlock(note: .F, block: 3),
+			NoteAtBlock(note: .G, block: 4),
+			NoteAtBlock(note: .A, block: 5),
+			NoteAtBlock(note: .B, block: 6),
+			NoteAtBlock(note: .A, block: 7),
+			NoteAtBlock(note: .G, block: 8),
+			NoteAtBlock(note: .F, block: 9),
+			NoteAtBlock(note: .E, block: 10),
+			NoteAtBlock(note: .D, block: 11),
+		]
+	])
 	override init(frame: CGRect) {
 		super.init(frame: frame)
 	}
@@ -100,18 +179,5 @@ class View: UIView {
 
 let view = View(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
 PlaygroundPage.current.liveView = view
+view.sequencer.prepareForPlaying()
 view.sequencer.start()
-view.sequencer.playNote(.A, onInstrument: .square)
-
-DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-	view.sequencer.playNote(.C, onInstrument: .square)
-	view.sequencer.playNote(.E, onInstrument: .square)
-}
-
-DispatchQueue.main.asyncAfter(deadline: .now() + 6) {
-	view.sequencer.stopNote(.A, onInstrument: .square)
-}
-
-DispatchQueue.main.asyncAfter(deadline: .now() + 9) {
-	view.sequencer.playNote(.A, onInstrument: .square)
-}
