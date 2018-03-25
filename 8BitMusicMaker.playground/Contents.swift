41b8e87859d1265286be5d52c6383b079020ead5
BenEmdon <benjaminemdon@gmail.com>
2018-03-25 12:15:29 -0400

    Moved Sequencer to it's own file

--- 8BitMusicMaker.playground/Contents.swift
+++ 8BitMusicMaker.playground/Contents.swift
@@ -3,189 +3,47 @@ import UIKit
 import Foundation
 import AVFoundation
 
-struct NoteAtBlock {
-	let note: Note
-	let block: Int
-}
-
-extension NoteAtBlock: Hashable {
-	static func ==(lhs: NoteAtBlock, rhs: NoteAtBlock) -> Bool {
-		return lhs.block == rhs.block
-			&& lhs.note == rhs.note
-	}
-	var hashValue: Int {
-		return "\(note.rawValue).\(block)".hashValue
-	}
-}
-
-protocol SequencerDelegate: class {
-	func blockChanged(_ block: Int)
-	func sequencerModeChanged(_ mode: Sequencer.Mode)
-}
-
-class Sequencer {
-
-	enum Mode {
-		case playing
-		case stopped
-	}
-
-	// Constants
-	let blocks = 14
-
-//	let blocksPerSecond: DispatchTime = 0.5
-
-	// AVFoundation dependancies
-	let engine = AVAudioEngine()
-
-	// Organized state of the sequencer
-	let players: [Instrument: [Note: PitchedPlayer]]
-	public private(set) var notesAtBlocks: [Instrument: Set<NoteAtBlock>]
-	var notesPlaying = [Instrument: Note]()
-	var currentMode: Mode = .stopped
-
-	weak var delegate: SequencerDelegate?
-
-	init(with instruments: [Instrument], initialState state: [Instrument: Set<NoteAtBlock>] = [:]) {
-		let buffers = Sequencer.audioBuffers(for: instruments)
-		players = Sequencer.createPlayers(forBuffers: buffers, engine: engine)
-		notesAtBlocks = state
-	}
-
-	// MARK: Static dispatched setup functions
-
-	private static func audioBuffers(for instruments: [Instrument]) -> [Instrument: AVAudioPCMBuffer] {
-		var audioBuffers = [Instrument: AVAudioPCMBuffer]()
-		for instrument in instruments {
-			let sample = instrument.sample
-			let audioBuffer = AVAudioPCMBuffer(pcmFormat: sample.processingFormat, frameCapacity: UInt32(sample.length))!
-			try! sample.read(into: audioBuffer)
-			audioBuffers[instrument] = audioBuffer
-		}
-		return audioBuffers
-	}
-
-	private static func createPlayers(forBuffers buffers: [Instrument: AVAudioPCMBuffer], engine: AVAudioEngine) -> [Instrument: [Note: PitchedPlayer]] {
-		var allPlayers = [Instrument: [Note: PitchedPlayer]]()
-		let instruments = buffers.map { instrument, _ in return instrument }
-		for instrument in instruments {
-			var notePlayers = [Note: PitchedPlayer]()
-			for note in Note.allValues {
-				notePlayers[note] = PitchedPlayer(engine: engine, audioBuffer: buffers[instrument]!, note: note)
-			}
-			allPlayers[instrument] = notePlayers
-		}
-		return allPlayers
-	}
-
-	// MARK: Sequencer behavior
-
-	func prepareForPlaying() {
-		engine.prepare()
-		try! engine.start()
-	}
-
-	func hardStop() {
-		currentMode = .stopped
-		for (instrument, notesAtBlocks) in notesAtBlocks {
-			let notesInBlock = notesAtBlocks
-				.map { noteAtBlock in noteAtBlock.note }
-			print(notesInBlock)
-			notesInBlock.forEach { note in stopNote(note, onInstrument: instrument) }
-		}
-	}
-
-	func start() {
-		currentMode = .playing
-		sequenceNotes(forNewBlock: 0, oldBlock: nil)
-	}
-
-	func sequenceNotes(forNewBlock newBlock: Int, oldBlock: Int?) {
-		if let oldBlock = oldBlock {
-			stopNotesForBlock(oldBlock)
-		}
-		guard currentMode == .playing else { return }
-		delegate?.blockChanged(newBlock)
-		playNotesForBlock(newBlock)
-		DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) { [weak self] in
-			guard let blocks = self?.blocks else { return }
-			let allButLastBlock = 0..<(blocks - 1)
-			if allButLastBlock.contains(newBlock) {
-				self?.sequenceNotes(forNewBlock: newBlock + 1, oldBlock: newBlock)
-			} else {
-				self?.sequenceNotes(forNewBlock: 0, oldBlock: newBlock)
-			}
-		}
-	}
-
-	private func playNotesForBlock(_ block: Int) {
-		for (instrument, notesAtBlocks) in notesAtBlocks {
-			let notesInBlock = notesAtBlocks
-				.filter { noteAtBlock in noteAtBlock.block == block }
-				.map { noteAtBlock in noteAtBlock.note }
-			notesInBlock.forEach { note in playNote(note, onInstrument: instrument) }
-
-		}
-	}
-
-	private func stopNotesForBlock(_ block: Int) {
-		for (instrument, notesAtBlocks) in notesAtBlocks {
-			let notesInBlock = notesAtBlocks
-				.filter { noteAtBlock in noteAtBlock.block == block }
-				.map { noteAtBlock in noteAtBlock.note }
-			notesInBlock.forEach { note in stopNote(note, onInstrument: instrument) }
-		}
-	}
-
-	func registerNote(_ note: Note, onInstrument instrument: Instrument, forBlock block: Int) {
-	}
-
-	func playNote(_ note: Note, onInstrument instrument: Instrument) {
-		// simple play a note
-		players[instrument]![note]!.play()
-	}
-
-	func stopNote(_ note: Note, onInstrument instrument: Instrument) {
-		// simple play a note
-		players[instrument]![note]!.stop()
-	}
-}
-
 class View: UIView {
-	let sequencer = Sequencer(with: [.square, .noise], initialState: [
-		.square: [
-			NoteAtBlock(note: .C2, block: 0),
-			NoteAtBlock(note: .D, block: 1),
-			NoteAtBlock(note: .E, block: 2),
-			NoteAtBlock(note: .F, block: 3),
-			NoteAtBlock(note: .G, block: 4),
-			NoteAtBlock(note: .A, block: 5),
-			NoteAtBlock(note: .B, block: 6),
-			NoteAtBlock(note: .C3, block: 7),
-			NoteAtBlock(note: .B, block: 8),
-			NoteAtBlock(note: .A, block: 9),
-			NoteAtBlock(note: .G, block: 10),
-			NoteAtBlock(note: .F, block: 11),
-			NoteAtBlock(note: .E, block: 12),
-			NoteAtBlock(note: .D, block: 13),
+	let sequencer = Sequencer(with: [.square, .triangle], initialState: [
+			.square: [
+	//			NoteAtBlock(note: .C2, block: 0),
+	//			NoteAtBlock(note: .C2, block: 1),
+	//			NoteAtBlock(note: .G, block: 2),
+	//			NoteAtBlock(note: .G, block: 3),
+	//			NoteAtBlock(note: .A, block: 4),
+	//			NoteAtBlock(note: .A, block: 5),
+	//			NoteAtBlock(note: .G, block: 6),
+
+	//			NoteAtBlock(note: .F, block: 8),
+	//			NoteAtBlock(note: .F, block: 9),
+	//			NoteAtBlock(note: .E, block: 10),
+	//			NoteAtBlock(note: .E, block: 11),
+	//			NoteAtBlock(note: .D, block: 12),
+	//			NoteAtBlock(note: .D, block: 13),
+	//			NoteAtBlock(note: .C2, block: 14),
+			],
+			.triangle: [
+				NoteAtBlock(note: .C2, block: 0),
+				NoteAtBlock(note: .C2, block: 1),
+				NoteAtBlock(note: .G, block: 2),
+				NoteAtBlock(note: .G, block: 3),
+				NoteAtBlock(note: .A, block: 4),
+				NoteAtBlock(note: .A, block: 5),
+				NoteAtBlock(note: .G, block: 6),
+
+				NoteAtBlock(note: .F, block: 8),
+				NoteAtBlock(note: .F, block: 9),
+				NoteAtBlock(note: .E, block: 10),
+				NoteAtBlock(note: .E, block: 11),
+				NoteAtBlock(note: .D, block: 12),
+				NoteAtBlock(note: .D, block: 13),
+				NoteAtBlock(note: .C2, block: 14),
+				
+			]
 		],
-		.noise: [
-			NoteAtBlock(note: .C2, block: 0),
-			NoteAtBlock(note: .D, block: 1),
-			NoteAtBlock(note: .E, block: 2),
-			NoteAtBlock(note: .F, block: 3),
-			NoteAtBlock(note: .G, block: 4),
-			NoteAtBlock(note: .A, block: 5),
-			NoteAtBlock(note: .B, block: 6),
-			NoteAtBlock(note: .C3, block: 7),
-			NoteAtBlock(note: .B, block: 8),
-			NoteAtBlock(note: .A, block: 9),
-			NoteAtBlock(note: .G, block: 10),
-			NoteAtBlock(note: .F, block: 11),
-			NoteAtBlock(note: .E, block: 12),
-			NoteAtBlock(note: .D, block: 13),
-		]
-	])
+		numberOfBlocks: 16
+	)
+
 	override init(frame: CGRect) {
 		super.init(frame: frame)
 	}
@@ -199,4 +57,5 @@ let view = View(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
 PlaygroundPage.current.liveView = view
 view.sequencer.prepareForPlaying()
 view.sequencer.start()
+
 
---
+++ 8BitMusicMaker.playground/Sources/Sequencer.swift
@@ -0,0 +1,168 @@
+import AVFoundation
+import Foundation
+
+public struct NoteAtBlock {
+	public let note: Note
+	public let block: Int
+	
+	public init(note: Note, block: Int) {
+		self.note = note
+		self.block = block
+	}
+}
+
+extension NoteAtBlock: Hashable {
+	public static func ==(lhs: NoteAtBlock, rhs: NoteAtBlock) -> Bool {
+		return lhs.block == rhs.block
+			&& lhs.note == rhs.note
+	}
+
+	public var hashValue: Int {
+		return "\(note.rawValue).\(block)".hashValue
+	}
+}
+
+public protocol SequencerDelegate: class {
+	func blockChanged(_ block: Int)
+	func sequencerModeChanged(_ mode: Sequencer.Mode)
+	func stateChanged(_ state: [Instrument: Set<NoteAtBlock>])
+}
+
+public class Sequencer {
+
+	public enum Mode {
+		case playing
+		case stopped
+	}
+
+	// Constants
+	let blocks: Int
+
+	//	let blocksPerSecond: DispatchTime = 0.5
+
+	// AVFoundation dependancies
+	private let engine = AVAudioEngine()
+
+	// Organized state of the sequencer
+	private let players: [Instrument: [Note: PitchedPlayer]]
+	public private(set) var notesAtBlocks: [Instrument: Set<NoteAtBlock>]
+	public private(set) var currentMode: Mode = .stopped
+
+	public weak var delegate: SequencerDelegate?
+
+	public init(with instruments: [Instrument], initialState state: [Instrument: Set<NoteAtBlock>] = [:], numberOfBlocks blocks: Int) {
+		let buffers = Sequencer.audioBuffers(for: instruments)
+		players = Sequencer.createPlayers(forBuffers: buffers, engine: engine)
+		notesAtBlocks = state
+		self.blocks = blocks
+	}
+
+	// MARK: Static dispatched setup functions
+
+	private static func audioBuffers(for instruments: [Instrument]) -> [Instrument: AVAudioPCMBuffer] {
+		var audioBuffers = [Instrument: AVAudioPCMBuffer]()
+		for instrument in instruments {
+			let sample = instrument.sample
+			let audioBuffer = AVAudioPCMBuffer(pcmFormat: sample.processingFormat, frameCapacity: UInt32(sample.length))!
+			try! sample.read(into: audioBuffer)
+			audioBuffers[instrument] = audioBuffer
+		}
+		return audioBuffers
+	}
+
+	private static func createPlayers(forBuffers buffers: [Instrument: AVAudioPCMBuffer], engine: AVAudioEngine) -> [Instrument: [Note: PitchedPlayer]] {
+		var allPlayers = [Instrument: [Note: PitchedPlayer]]()
+		let instruments = buffers.map { instrument, _ in return instrument }
+		for instrument in instruments {
+			var notePlayers = [Note: PitchedPlayer]()
+			for note in Note.allValues {
+				notePlayers[note] = PitchedPlayer(engine: engine, audioBuffer: buffers[instrument]!, note: note)
+			}
+			allPlayers[instrument] = notePlayers
+		}
+		return allPlayers
+	}
+
+	// MARK: Sequencer behavior
+
+	public func prepareForPlaying() {
+		engine.prepare()
+		try! engine.start()
+	}
+
+	public func hardStop() {
+		currentMode = .stopped
+		for (instrument, notesAtBlocks) in notesAtBlocks {
+			let notesInBlock = notesAtBlocks
+				.map { noteAtBlock in noteAtBlock.note }
+			print(notesInBlock)
+			notesInBlock.forEach { note in stopNote(note, onInstrument: instrument) }
+		}
+		delegate?.sequencerModeChanged(currentMode)
+		delegate?.blockChanged(0)
+	}
+
+	public func start() {
+		currentMode = .playing
+		delegate?.sequencerModeChanged(currentMode)
+		sequenceNotes(forNewBlock: 0, oldBlock: nil)
+	}
+
+	private func sequenceNotes(forNewBlock newBlock: Int, oldBlock: Int?) {
+		if let oldBlock = oldBlock {
+			stopNotesForBlock(oldBlock)
+		}
+		guard currentMode == .playing else { return }
+		delegate?.blockChanged(newBlock)
+		playNotesForBlock(newBlock)
+		DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) { [weak self] in
+			guard let blocks = self?.blocks else { return }
+			let allButLastBlock = 0..<(blocks - 1)
+			if allButLastBlock.contains(newBlock) {
+				self?.sequenceNotes(forNewBlock: newBlock + 1, oldBlock: newBlock)
+			} else {
+				self?.sequenceNotes(forNewBlock: 0, oldBlock: newBlock)
+			}
+		}
+	}
+
+	private func playNotesForBlock(_ block: Int) {
+		for (instrument, notesAtBlocks) in notesAtBlocks {
+			let notesInBlock = notesAtBlocks
+				.filter { noteAtBlock in noteAtBlock.block == block }
+				.map { noteAtBlock in noteAtBlock.note }
+			notesInBlock.forEach { note in playNote(note, onInstrument: instrument) }
+
+		}
+	}
+
+	private func stopNotesForBlock(_ block: Int) {
+		for (instrument, notesAtBlocks) in notesAtBlocks {
+			let notesInBlock = notesAtBlocks
+				.filter { noteAtBlock in noteAtBlock.block == block }
+				.map { noteAtBlock in noteAtBlock.note }
+			notesInBlock.forEach { note in stopNote(note, onInstrument: instrument) }
+		}
+	}
+
+	public func registerNote(_ note: Note, onInstrument instrument: Instrument, forBlock block: Int) {
+		notesAtBlocks[instrument]?.insert(NoteAtBlock(note: note, block: block))
+		delegate?.stateChanged(notesAtBlocks)
+	}
+
+	public func deregisterNote(_ note: Note, onInstrument instrument: Instrument, forBlock block: Int) {
+		if let _ = notesAtBlocks[instrument]?.remove(NoteAtBlock(note: note, block: block)) {
+			delegate?.stateChanged(notesAtBlocks)
+		}
+	}
+
+	private func playNote(_ note: Note, onInstrument instrument: Instrument) {
+		// simple play a note
+		players[instrument]![note]!.play()
+	}
+
+	private func stopNote(_ note: Note, onInstrument instrument: Instrument) {
+		// simple play a note
+		players[instrument]![note]!.stop()
+	}
+}
