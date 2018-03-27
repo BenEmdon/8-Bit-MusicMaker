dea9d3ab33626446ad98f0fddeb5ac49905342f4
BenEmdon <benjaminemdon@gmail.com>
2018-03-27 16:21:15 -0400

    Added resources section

--- 8BitMusicMaker.playground/Pages/Info.xcplaygroundpage/Contents.swift
+++ 8BitMusicMaker.playground/Pages/Info.xcplaygroundpage/Contents.swift
@@ -3,10 +3,13 @@
 [👉 Demo](@next)
 
 # About:
-Feel nostalgic when you hear an old video games music? If so, 8-Bit Music Maker is here to let you re-live those memories! It's packed with the 8-Bit sounds from the past, and filled with endless possible songs (some creativity required)! This playground app is the perfect place to be creative, and bring out your inner childhood.
+Feel nostalgic when you hear an old video game's music? If so, 8-Bit Music Maker is here to let you re-live those memories! It's packed with 8-Bit sounds from the past, and filled with endless possibilities of songs (some creativity required)! This playground is the perfect place to be creative, and bring out your inner child.
 
 ### Inspiration:
-_While I wasn't born in the 80's, growing up I spent a lot of my childhood playing older generation videogames as they were handed down to me. Some of these games have brought me joy to this day!_
+_While I wasn't born in the 80's, growing up I spent a lot of my childhood playing older generation videogames which were handed down to me. Some of these games have brought me joy to this day!_
+
+### Resources:
+The resources found in this playground were all created by me. I developed the C note samples in [Garage Band.app](https://www.apple.com/ca/mac/garageband/) using an [8-Bit plugin](http://www.ymck.net/en/download/magical8bitplug/). I drew all the pixel art in [Pixen.app](http://itunes.apple.com/us/app/pixen/id525180431?mt=12) which is avaliable on the Mac App Store.
 
 ### Author:
 [Ben Emdon 👨‍💻](https://github.com/BenEmdon)
