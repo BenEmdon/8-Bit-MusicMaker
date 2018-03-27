67b3d8e7bc5ed23f0bd1bdd2edcd91b13f267eab
BenEmdon <benjaminemdon@gmail.com>
2018-03-27 15:08:44 -0400

    Added Mario theme demo

--- 8BitMusicMaker.playground/Contents.swift
+++ 8BitMusicMaker.playground/Contents.swift
@@ -4,13 +4,45 @@ import AVFoundation
 
 let path = playgroundSharedDataDirectory.appendingPathComponent("music.caf")
 
+let marioTheme: Set<NoteAtBlock> = [
+	NoteAtBlock(note: .D, block: 0),
+	NoteAtBlock(note: .E2, block: 0),
+	NoteAtBlock(note: .D, block: 1),
+	NoteAtBlock(note: .E2, block: 1),
+	NoteAtBlock(note: .D, block: 3),
+	NoteAtBlock(note: .E2, block: 3),
+	NoteAtBlock(note: .D, block: 5),
+	NoteAtBlock(note: .C2, block: 5),
+	NoteAtBlock(note: .D, block: 6),
+	NoteAtBlock(note: .E2, block: 6),
+	NoteAtBlock(note: .G, block: 8),
+	NoteAtBlock(note: .G2, block: 8),
+	NoteAtBlock(note: .G, block: 11),
+	NoteAtBlock(note: .E, block: 14),
+	NoteAtBlock(note: .G, block: 14),
+	NoteAtBlock(note: .C2, block: 14),
+	NoteAtBlock(note: .E, block: 16),
+	NoteAtBlock(note: .G, block: 16),
+	NoteAtBlock(note: .E, block: 19),
+	NoteAtBlock(note: .C, block: 19),
+	NoteAtBlock(note: .F, block: 21),
+	NoteAtBlock(note: .A, block: 21),
+	NoteAtBlock(note: .B, block: 23),
+	NoteAtBlock(note: .G, block: 23),
+	NoteAtBlock(note: .B, block: 25),
+	NoteAtBlock(note: .G, block: 25),
+	NoteAtBlock(note: .F, block: 26),
+	NoteAtBlock(note: .A, block: 26),
+]
+
+
 let bitMusicMaker = BitMusicMaker(
-	with: [.triangle, .square],
-	initialState: [:],
-	numberOfBlocks: 10,
-	beatsPerMinute: 140,
+	with: [.square],
+	initialState: [.square: marioTheme],
+	numberOfBlocks: 30,
+	beatsPerMinute: 300,
 	saveURL: path,
-	numberOfOctaves: .one
+	numberOfOctaves: .two
 )
 PlaygroundPage.current.liveView = bitMusicMaker
 
